0ac46a8bb4b396379e009cb26713d61208c5e23d
OkayHughes <OkayHughes@gmail.com>
2014-09-24 01:38:23 -0400

    All of the output is broken and there is no sanitization. It sort of works, though.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,10 +12,19 @@ class ArticlesController < ApplicationController
   	rescue
   		proc_id = Digest::SHA1.hexdigest("1")
   	end
-  	@job = Job.create(filepath: File.join("/public/data", params[:script].original_filename),
+  	@job = Job.create(filepath: File.join(Rails.root, "public", "data" , params[:script].original_filename),
   	    article_id: params[:article_id],
   	    p_id: proc_id)
   	@job.save
+  	@result = ""
+  	File.open(@job.filepath, 'r') do |f|
+  		while line = f.gets 
+  			res = IO.popen(line)
+  			@result += res.readlines.to_s
+  			res.readlines
+  		end
+  	end
+
   	respond_to do |format|
   		format.js
   	end
