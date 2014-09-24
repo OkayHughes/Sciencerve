2ea9d55bd0833c2b173b9fae61a0b0d2643f0c47
OkayHughes <OkayHughes@gmail.com>
2014-09-24 01:17:12 -0400

    No idea whether it works or not, but the basic functionality is there.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,14 @@ class ArticlesController < ApplicationController
 
   def upload
   	ProcessFile.save(params[:script])
+  	begin 
+  		proc_id = Digest::SHA1.hexdigest(Job.order("created_at").last.p_id)
+  	rescue
+  		proc_id = Digest::SHA1.hexdigest("1")
+  	end
   	@job = Job.create(filepath: File.join("/public/data", params[:script].original_filename),
   	    article_id: params[:article_id],
-  	    p_id: Digest::SHA1.hexdigest(Job.order("created_at").last.p_id || 1))
+  	    p_id: proc_id)
   	@job.save
   	respond_to do |format|
   		format.js
